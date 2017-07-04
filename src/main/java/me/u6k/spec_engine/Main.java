67616c72b20937247db3cbe2715e57cb347226b8
u6k <u6k.apps@gmail.com>
2017-07-05 02:54:05 +0900

    文字列としてソースコードを読み込み、構文解析して標準出力するだけのソースコードを書いてみました。

--- src/main/java/me/u6k/spec_engine/Main.java
+++ src/main/java/me/u6k/spec_engine/Main.java
@@ -1,6 +1,24 @@
 
 package me.u6k.spec_engine;
 
+import java.util.List;
+
+import me.u6k.spec_engine.parser.SampleBaseVisitor;
+import me.u6k.spec_engine.parser.SampleLexer;
+import me.u6k.spec_engine.parser.SampleParser;
+import me.u6k.spec_engine.parser.SampleParser.ElementContext;
+import me.u6k.spec_engine.parser.SampleParser.ExprAssignContext;
+import me.u6k.spec_engine.parser.SampleParser.ExprContext;
+import me.u6k.spec_engine.parser.SampleParser.FormulaAssignContext;
+import me.u6k.spec_engine.parser.SampleParser.FormulaContext;
+import me.u6k.spec_engine.parser.SampleParser.ProgContext;
+import me.u6k.spec_engine.parser.SampleParser.StatContext;
+import me.u6k.spec_engine.parser.SampleParser.TupleAssignContext;
+import me.u6k.spec_engine.parser.SampleVisitor;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +27,89 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    public Object parse(String text) {
+        CharStream stream = CharStreams.fromString(text);
+        SampleLexer lexer = new SampleLexer(stream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SampleParser parser = new SampleParser(tokens);
+        SampleVisitor visitor = new SampleVisitorImpl();
+        Object result = visitor.visit(parser.prog());
+
+        return result;
+    }
+
+    private class SampleVisitorImpl extends SampleBaseVisitor<Object> {
+
+        @Override
+        public Object visitProg(ProgContext ctx) {
+            System.out.println("visitProg:");
+            this.printTokens(ctx.children);
+
+            return super.visitProg(ctx);
+        }
+
+        @Override
+        public Object visitStat(StatContext ctx) {
+            System.out.println("visitStat:");
+            this.printTokens(ctx.children);
+
+            return super.visitStat(ctx);
+        }
+
+        @Override
+        public Object visitExprAssign(ExprAssignContext ctx) {
+            System.out.println("visitExprAssign: ID=" + ctx.ID());
+            this.printTokens(ctx.children);
+
+            return super.visitExprAssign(ctx);
+        }
+
+        @Override
+        public Object visitFormulaAssign(FormulaAssignContext ctx) {
+            System.out.println("visitFormulaAssign: ID=" + ctx.ID());
+            this.printTokens(ctx.children);
+
+            return super.visitFormulaAssign(ctx);
+        }
+
+        @Override
+        public Object visitTupleAssign(TupleAssignContext ctx) {
+            System.out.println("visitTupleAssign: ID=" + ctx.ID());
+            this.printTokens(ctx.children);
+
+            return super.visitTupleAssign(ctx);
+        }
+
+        @Override
+        public Object visitElement(ElementContext ctx) {
+            System.out.println("visitElement:");
+            this.printTokens(ctx.children);
+
+            return super.visitElement(ctx);
+        }
+
+        @Override
+        public Object visitExpr(ExprContext ctx) {
+            System.out.println("visitExpr: ID=" + ctx.ID() + ", INT=" + ctx.INT() + ", ADD=" + ctx.ADD() + ", SUB=" + ctx.SUB() + ", MUL=" + ctx.MUL() + ", DIV=" + ctx.DIV());
+            this.printTokens(ctx.children);
+
+            return super.visitExpr(ctx);
+        }
+
+        @Override
+        public Object visitFormula(FormulaContext ctx) {
+            System.out.println("visitFormula:");
+            this.printTokens(ctx.children);
+
+            return super.visitFormula(ctx);
+        }
+
+        private void printTokens(List<ParseTree> ctx) {
+            ctx.forEach(x -> System.out.println("  child=[" + x.getClass().getName() + ", " + x + "]"));
+        }
+
     }
 
 }
--- src/test/java/me/u6k/spec_engine/MainTest.java
+++ src/test/java/me/u6k/spec_engine/MainTest.java
@@ -2,11 +2,48 @@
 package me.u6k.spec_engine;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MainTest {
 
     @Test
-    public void test() {
+    public void test1() {
+        String text = "a = 100;";
+        new Main().parse(text);
+    }
+
+    @Test
+    public void test2() {
+        String text = "b = 100 * 2;";
+        new Main().parse(text);
+    }
+
+    @Test
+    public void test3() {
+        String text = "c = 100 - 20 * 3;";
+        new Main().parse(text);
+    }
+
+    @Test
+    public void test4() {
+        String text = "d = true;";
+        new Main().parse(text);
+    }
+
+    @Test
+    public void test5() {
+        String text = "e = true && false;";
+        new Main().parse(text);
+    }
+
+    @Test
+    public void test6() {
+        String text = "f = true -> true && false;";
+        new Main().parse(text);
     }
 
 }
